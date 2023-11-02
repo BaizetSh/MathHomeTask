9ba3425f915773eac8d8e96db1c7d516bf757baa
BaizetSh <98802230+BaizetSh@users.noreply.github.com>
2023-11-02 21:06:52 +0300

    Add files via upload

---
+++ MathHomeTask/MathHomeTask/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathHomeTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            Console.WriteLine("Введите x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            double A = 2 * Math.Cos(x - (Math.PI / 6));
+
+            double y;
+            Console.WriteLine("Введите y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+            double B = 0.5 + Math.Pow(Math.Sin(y), 2);
+
+            double z;
+            Console.WriteLine("Введите z: ");
+            z = Convert.ToDouble(Console.ReadLine());
+            double C = 1 + (Math.Pow(z, 2) / 3 - Math.Pow(z, 2) / 5);
+
+            double t = A / B * C;
+
+            Console.WriteLine("t = " + t);
+            Console.ReadKey();
+
+        }
+
+    }
+}
